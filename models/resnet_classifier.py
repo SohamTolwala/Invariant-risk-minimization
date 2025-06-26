f47616d5ec1ad4926d9dfb6043b222f2a1aee6f1
Soham Tolwala <94548689+SohamTolwala@users.noreply.github.com>
2025-06-26 18:39:40 +0530

    Update resnet_classifier.py

--- models/resnet_classifier.py
+++ models/resnet_classifier.py
@@ -2,11 +2,13 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
-
 class ResNet50Classifier(nn.Module):
-    def __init__(self, num_classes=2, pretrained=True):
+    def __init__(self, num_classes=2):
         super().__init__()
-        self.backbone = models.resnet50(pretrained=pretrained)
+        
+        self.backbone = models.resnet50(pretrained=False)
+
+        
         in_features = self.backbone.fc.in_features
         self.backbone.fc = nn.Linear(in_features, num_classes)
 
